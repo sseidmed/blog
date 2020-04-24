5f112c946d7fd0a7120680ce500d449cf47e1f31
vagrant <vagrant@vagrant.vm>
2020-04-24 22:29:51 +0000

    active record associations added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  has_many :comments #this will take comments and add a method with the same name to post model objects
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
-  get '/pages', to: 'pages#index', as: 'pages'
-  post '/pages', to: 'pages#create'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to: 'pages#show', as: 'page'
-  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  patch '/pages/:id', to: 'pages#update'
-  delete '/pages/:id', to: 'pages#destroy'
-  #resources :pages (shortcut to all the above 7 lines of code)
+  # get '/pages', to: 'pages#index', as: 'pages'
+  # post '/pages', to: 'pages#create'
+  # get '/pages/new', to: 'pages#new', as: 'new_page'
+  # get '/pages/:id', to: 'pages#show', as: 'page'
+  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  # patch '/pages/:id', to: 'pages#update'
+  # delete '/pages/:id', to: 'pages#destroy'
+
+  resources :pages #(shortcut to all the above 7 lines of code)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
